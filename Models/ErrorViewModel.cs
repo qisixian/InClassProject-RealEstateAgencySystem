65b874095181e7554eefc1d56991fb5b8fe5e9ba
Sixian <2235127438@qq.com>
2025-03-12 15:17:35 -0700

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RealEstateAgencySystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
