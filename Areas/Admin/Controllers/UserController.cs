321ce27cfefceaac0a3e38c8b6944ebcf1c9e8d1
Sixian <2235127438@qq.com>
2025-04-16 09:37:58 -0700

    update

---
+++ Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using RealEstateAgencySystem.Models;
+using RealEstateAgencySystem.Data;
+using RealEstateAgencySystem.Areas.Admin.Views;
+
+namespace RealEstateAgencySystem.Areas.Admin.Controllers
+{
+    // [Authorize(Roles = "Admin")]
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        // private AppDbContext context;
+        // public ContractController(AppDbContext ctx) => context = ctx;
+
+        private Repository<Property> data { get; set; }
+
+        public UserController(AppDbContext ctx)
+        {
+            data = new Repository<Property>(ctx);
+        }
+
+        public RedirectToActionResult Index() => RedirectToAction("List");
+
+
+        public ViewResult List()
+        {
+            return View();
+        }
+    }
+}
